3a65bfb8cafddc3d350800cfcedb3c2162a79b54
Bodrami <dlwldms1523@gmail.com>
2024-08-01 20:39:00 +0900

    Commit Test

---
+++ JustDoIt/src/main/java/com/project/justdoit/JustDoItApplication.java
@@ -0,0 +1,13 @@
+package com.project.justdoit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustDoItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustDoItApplication.class, args);
+    }
+
+}
---
+++ JustDoIt/src/test/java/com/project/justdoit/JustDoItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.justdoit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustDoItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
